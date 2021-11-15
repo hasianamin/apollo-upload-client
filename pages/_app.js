aabbe67dbf843fc45c35ec9f5c80a205a9938b6b
hasianamin <hasianamin14@gmail.com>
2021-11-15 09:34:11 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+import { createUploadLink } from 'apollo-upload-client';
+import App from './index'
+
+const client = new ApolloClient({
+  link: createUploadLink({
+    uri: 'http://localhost:8000/graphql'
+  }),
+  cache: new InMemoryCache()
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+
+  
+  return (
+    <ApolloProvider client={client}>
+      <App/>
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
